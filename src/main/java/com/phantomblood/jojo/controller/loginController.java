a2b08f99fda8af6367a62ca51e2cf9710c1c858f
gaoxianliang <gaoxl@fapiao.com>
2020-07-20 10:02:33 +0800

    add html change controller use thymeleaf

--- src/main/java/com/phantomblood/jojo/controller/loginController.java
+++ src/main/java/com/phantomblood/jojo/controller/loginController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
@@ -19,9 +20,8 @@ public class loginController {
     private testService testService;
 
     @RequestMapping("/")
-    @ResponseBody
-    public String login(){
-        return "helloworld";
+    public ModelAndView login() {
+        return new ModelAndView("login");
     }
 
     @RequestMapping("/user")
